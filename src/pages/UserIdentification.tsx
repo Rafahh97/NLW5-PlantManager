6cc7d7cc6e61d79e49ec348e0ec7f1a04d31d28d
Rafaela Gomes <rafahh97@gmail.com>
2021-04-21 09:22:22 -0300

    improve page button and input

--- src/pages/UserIdentification.tsx
+++ src/pages/UserIdentification.tsx
@@ -6,22 +6,40 @@ import {
     Text, 
     TextInput,
     KeyboardAvoidingView,
+    TouchableWithoutFeedback,
     Platform,
+    Keyboard,
 } from 'react-native';
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
 import {Button} from '../components/Button';
+import { useNavigation } from '@react-navigation/core';
 
 export function UserIdentification(){
     const [isFocused, setIsFocused] = useState(false);
+    const [isFilled, setIsFilled] = useState(false);
+    const [name, setName] = useState<string>(); 
+
+    const navigation = useNavigation()
+
 
     function handleInputBlur(){
         setIsFocused(false)
+        setIsFilled(!!name)
     }
 
     function handleInputFocus(){
         setIsFocused(true)
+    }
+
+    function handleInputChange(value: string){
+        setIsFilled(!!value)
+        setName(value)
+    }
+
+    function handleSubmit(){
+        navigation.navigate('Confirmation');
     }
 
     return(
@@ -30,12 +48,13 @@ export function UserIdentification(){
             style = {styles.container}
             behavior = {Platform.OS == 'ios' ? 'padding' : 'height'}
             >
+                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
                 <View style={styles.content}>
-                    <View style={styles.header}>
-                        <View style={styles.form}>
+                    <View style={styles.form}>
+                        <View style={styles.header}>
 
                          <Text style={styles.emoji}>
-                           😃  
+                           {isFilled ? '😄' : '😃'}  
                          </Text>
                          <Text style={styles.title}>
                              Como podemos {'\n'}
@@ -46,19 +65,24 @@ export function UserIdentification(){
                       <TextInput 
                         style={[
                           styles.imput,
-                          isFocused && {borderColor: colors.green}
+                          (isFocused || isFilled) && {borderColor: colors.green}
                         ]}
                         placeholder="Digite seu nome"
                         onBlur={handleInputBlur}
                         onFocus={handleInputFocus} 
+                        onChangeText={handleInputChange}
                       />
 
                       <View style={styles.footer}>
-                           <Button/>
+                           <Button
+                           title="Confirmar"
+                           onPress={handleSubmit}
+                           />
                       </View>
 
                     </View>
                 </View>
+                </TouchableWithoutFeedback>
             </KeyboardAvoidingView>
         </SafeAreaView>
     )
